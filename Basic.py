2f582a2457dcaedcecf794aa0adee8f6a57e0de5
ummetarin <ummehomairatarin@gmail.com>
2024-10-15 00:12:24 +0600

    if/else

--- Basic.py
+++ Basic.py
@@ -1,5 +1,15 @@
 # variable
-a=10
-b=30
-sum=a+b
-print(sum)
+# a=10
+# b=30
+# sum=a+b
+# print(sum)
+
+#if/else
+
+n1=20
+if(n1 <2):
+    print("greater")
+elif(n1==2):
+    print("equal")
+else:
+    print("lower")
